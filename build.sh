be0b474a199af4bf54f9d5b39f941eb60647cf4e
Joshua <jabarcap.2004@gmail.com>
2026-05-14 18:47:40 -0500

    Automatizar creación de superusuario

--- build.sh
+++ build.sh
@@ -4,3 +4,7 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Crear superusuario automáticamente si no existe
+# El "|| true" al final es un truco para que no dé error si el usuario ya fue creado en el futuro
+python manage.py createsuperuser --noinput --username admin_jared --email jabarcap.2004@gmail.com || true
